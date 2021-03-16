a93ec0a2c2a63d12a93134170e8fd3bdf1052309
Sadeck33 <80730237+Sadeck33@users.noreply.github.com>
2021-03-15 20:36:14 -0600

    Add files via upload

---
+++ Calcu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculos
+{
+    public class Calcu
+    {
+        public double Num01 { get; set; }
+        public double Num02 { get; set; }
+        public double Cuadrado()
+        {
+            return Num01 * Num01;
+        }
+        public double Circulo(double N01)
+        {
+            return (3.1416 * N01 * N01);
+        }
+        public double Triangulo(double N01, double N02)
+        {
+            Num01 = N01;
+            Num02 = N02;
+            var Resul = ((N01 * N02) / 2);
+            return Resul;
+        }
+    }
+}
---
+++ Calculator.asmx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Calculos
+{
+    /// <summary>
+    /// Descripción breve de Calculator
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Calculator : System.Web.Services.WebService
+    {
+        Calcu c = new Calcu();
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        [WebMethod]
+        public double Cuadrado(double N01, double N02)
+        {
+            c.Num01 = N01;
+            return c.Cuadrado();
+        }
+        [WebMethod]
+        public double Circulo(double N01)
+        {
+            return c.Circulo(N01);
+        }
+        [WebMethod]
+        public double Triangulo(double N01, double N02)
+        {
+            return c.Triangulo(N01, N02);
+        }
+    }
+}
